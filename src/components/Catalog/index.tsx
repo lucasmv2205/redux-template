ef1064443743b51e005a103416047ba19c4537de
lucasmv2205 <lmartins2205@gmail.com>
2022-11-17 23:35:44 -0300

    start a card app with redux

---
+++ src/components/Catalog/index.tsx
@@ -0,0 +1,35 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import api from '../../services/api'
+import { addProductToCart } from '../../store/modules/cart/actions'
+import { IProduct } from '../../store/modules/cart/types'
+
+const Catalog = () => {
+    const dispatch = useDispatch();
+    const [catalog, setCatalog] = useState<IProduct[]>([])
+
+    useEffect(() => {
+        api.get('products').then((response) => {
+            setCatalog(response.data)
+        })
+    },[]);
+
+    const handleAddProductToCart = useCallback((product: IProduct) => {
+        dispatch(addProductToCart(product));
+    },[dispatch])
+    
+    return(
+        <div>
+            <h1>Catalog</h1>
+            {catalog.map(product => (
+                <article key={product.id}>
+                    <strong>{product.title}--</strong>
+                    <span>--{product.price}</span>
+                    <button onClick={() => handleAddProductToCart(product)} type='button'>comprar</button>
+                </article>
+            ))}
+        </div>
+    )
+}
+
+export default Catalog
